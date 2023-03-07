959ca1e3dd317713d926b9dfbe96e026a63d4983
EduSTC <edustclair@gmail.com>
2023-03-07 10:28:31 -0300

    Added images to menu and created the logic from the squares

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-canvasSquares(16);
+// createSquares(16);
 const color = "black";
+let lastColor = color;
 
 
 let mouseIsDown = false;
@@ -8,25 +9,31 @@ document.body.addEventListener('mousedown', () => {
     mouseIsDown = true;
 });
 
+
 document.body.addEventListener('mouseup', () => {
     mouseIsDown = false;
 });
 
 
-// https://www.w3schools.com/jsref/dom_obj_event.asp
-// https://stackoverflow.com/questions/47641309/combine-mouse-events-in-javascript
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mousemove', () => {
-        if (mouseIsDown == true) {
-            square.setAttribute('style', 'background-color: ' + color);
-        }
-    });
-})
+let slider = document.getElementById('slider');
+let value = document.getElementById('grid-value');
+value.textContent = slider.value + " x " + slider.value;
+createSquares(slider.value);
+
+slider.oninput = function() {
+    value.textContent = this.value + " x " + this.value;
+    createSquares(this.value);
+}
 
 
-function canvasSquares(num) {
+
+
+
+
+
+function createSquares(num) {
     let containerCanvas = document.getElementById('canvas');
+    resetSquares(containerCanvas);
     for (let i = 0; i < num; i++) {
         const column = document.createElement('div');
         column.setAttribute('class', 'column');
@@ -37,6 +44,27 @@ function canvasSquares(num) {
             column.appendChild(row);
         }
     }
+    enablePainting();
 }
+
+// https://www.w3schools.com/jsref/dom_obj_event.asp
+// https://stackoverflow.com/questions/47641309/combine-mouse-events-in-javascript
+function enablePainting() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mousemove', () => {
+            if (mouseIsDown == true) {
+                square.setAttribute('style', 'background-color: ' + color);
+            }
+        });
+    })
+}
+
+function resetSquares(squareArea) {
+    while (squareArea.firstChild) {
+        squareArea.removeChild(squareArea.firstChild);
+    }
+}
+
 
 
